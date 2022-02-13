2cfdf126fcf6898a40f6e83cf450b6ec4f724573
efeichelberger <48500377+efeichelberger@users.noreply.github.com>
2022-02-13 02:54:47 -0500

    changes to window size and position

--- window.py
+++ window.py
@@ -4,7 +4,17 @@ import tkinter as tk
 def create_window():
     root = tk.Tk()
     root.title("GIF Creator")
-    root.geometry('600x400+600+400')
+
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    center_x = int(screen_width / 2 - 300)
+    center_y = int(screen_height / 2 - 200)
+
+    root.geometry(f'600x400+{center_x}+{center_y}')
 
     root.mainloop()
 
+
+# def get_coordinates()
+# def get_user_delay()
